f5380dd203273b5a23a4783800e2a4a25a507337
Sandeep <sandeepyalamati1999@gmail.com>
2025-07-26 23:05:16 +0530

    netflix-gpt

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>
+        Hellow World !
+      </div>
+
+    </>
+  )
+}
+
+export default App
